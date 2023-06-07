bbcc5919164a5212c2158b0c98a1469c4a298f07
CDonekhan <cbreuer10@gmaill.com>
2023-06-07 16:11:05 -0700

    Fixed

--- src/Library/Objects/ShapeRenderer.cpp
+++ src/Library/Objects/ShapeRenderer.cpp
@@ -45,3 +45,10 @@ ShapeRenderer::~ShapeRenderer()
  * External Functionality
  *============================================================================================================================================================================================*/
 
+void ShapeRenderer::addLightSource(glm::vec3 color)
+{
+	this->lightSource = LightSource(this->orientation->translation, color);
+	this->shader = &Resources::shaderManager.lightSourceShader;
+	lightSource.parentObject = LightSource::OBJECT;
+	Resources::currentApplication->allLightSources.push_back(&lightSource);
+}
