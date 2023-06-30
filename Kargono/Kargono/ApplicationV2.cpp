ffee567cbd110ea0a19f3c3d31fd628e579c8e3c
Charles <cbreuer10@gmail.com>
2023-06-30 09:32:31 -0700

    Updating Application Structure

---
+++ Kargono/Kargono/ApplicationV2.cpp
@@ -0,0 +1,20 @@
+#include "ApplicationV2.h"
+
+namespace Karg
+{
+
+	ApplicationV2::ApplicationV2()
+	{
+		
+	}
+	ApplicationV2::~ApplicationV2()
+	{
+	
+	}
+
+	void ApplicationV2::Run()
+	{
+		while (true);
+	}
+
+}
---
+++ Kargono/Kargono/ApplicationV2.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Karg
+{
+	class ApplicationV2
+	{
+	public:
+		ApplicationV2();
+		~ApplicationV2();
+		void Run();
+	};
+}
+
