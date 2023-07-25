54bf9af5374eb84cf9027614967680aac86c633d
Charles <cbreuer10@gmail.com>
2023-07-24 20:22:46 -0700

    WORKIN

---
+++ dependencies/custom-documentation-generator/directories/Kargono-cache-dir/Kargono/Renderer/RendererrAPI.cpp
@@ -0,0 +1,7 @@
+#include "Kargono/kgpch.h"
+#include "Kargono/Renderer/RendererAPI.h"
+
+namespace Kargono
+{
+	RendererAPI::API RendererAPI::s_API = RendererAPI::API::OpenGL;
+}
---
+++ dependencies/custom-documentation-generator/openai_keys.py
@@ -0,0 +1,3 @@
+
+
+api_key = "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"
