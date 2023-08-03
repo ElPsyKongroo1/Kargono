29cc93130e07db55ca70fa33d4eba3f74737c1ad
Charles <cbreuer10@gmail.com>
2023-08-02 21:02:40 -0700

    New message for the combined commit

--- Kargono/Kargono/Core/Core.h
+++ Kargono/Kargono/Core/Core.h
@@ -45,15 +45,25 @@
 #endif // End of platform detection
 
 #ifdef KG_DEBUG
-#define KG_ENABLE_ASSERTS
+	#if defined(KG_PLATFORM_WINDOWS)
+		#define KG_DEBUGBREAK() __debugbreak()
+	#elif defined(KG_PLATFORM_LINUX)
+		#include <signal.h>
+		#define KG_DEBUGBREAK() raise(SIGTRAP)
+	#else
+		#error "Platform doesn't support debugbreak yet!"
+	#endif
+	#define KG_ENABLE_ASSERTS
+#else
+#define KG_DEBUGBREAK()
 #endif
 
 #ifdef KG_ENABLE_ASSERTS
-#define KG_ASSERT(x, ...) { if(!(x)) { KG_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-#define KG_CORE_ASSERT(x, ...) { if(!(x)) { KG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define KG_ASSERT(x, ...) { if(!(x)) { KG_ERROR("Assertion Failed: {0}", __VA_ARGS__); KG_DEBUGBREAK(); } }
+	#define KG_CORE_ASSERT(x, ...) { if(!(x)) { KG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); KG_DEBUGBREAK(); } }
 #else
-#define KG_ASSERT(x, ...)
-#define KG_CORE_ASSERT(x, ...)
+	#define KG_ASSERT(x, ...)
+	#define KG_CORE_ASSERT(x, ...)
 #endif
 
 #define BIT(x) (1 << x)
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -8,7 +8,7 @@
 #include "Platform/OpenGL/OpenGLShader.h"
 
 Sandbox2D::Sandbox2D()
-	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true)
+	: Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f, true), m_SquareColor({0.2f, 0.3f, 0.8f, 1.0f})
 {
 	
 }
@@ -36,6 +36,8 @@ void Sandbox2D::OnUpdate(Kargono::Timestep ts)
 
 
 	//Render
+	Kargono::Renderer2D::ResetStats();
+
 	{
 		KG_PROFILE_SCOPE("Renderer Prep::OnUpdate");
 		Kargono::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
@@ -43,13 +45,27 @@ void Sandbox2D::OnUpdate(Kargono::Timestep ts)
 	}
 
 	{
+		static float rotation = 0.0f;
+		rotation += ts * 50.0f;
+
 		KG_PROFILE_SCOPE("Renderer Draw::OnUpdate");
 		Kargono::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		//Kargono::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f },glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		Kargono::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f },-45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Kargono::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-		Kargono::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Kargono::Renderer2D::DrawQuad({ -5.0f, -5.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
-		Kargono::Renderer2D::DrawQuad({ -0.5f, -0.5f, 0.0f }, { 1.0f, 1.0f }, m_CheckerboardTexture, 20.0f);
+		Kargono::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, m_SquareColor);
+		Kargono::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
+		Kargono::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		Kargono::Renderer2D::EndScene();
+
+		Kargono::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { x + 5.0f / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.5f};
+				Kargono::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		Kargono::Renderer2D::EndScene();
 	}
 }
@@ -59,6 +75,14 @@ void Sandbox2D::OnImGuiRender()
 	KG_PROFILE_FUNCTION();
 
 	ImGui::Begin("Settings");
+
+	auto stats = Kargono::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d:", stats.DrawCalls);
+	ImGui::Text("Quads: %d:", stats.QuadCount);
+	ImGui::Text("Vertices: %d:", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d:", stats.GetTotalIndexCount());
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
