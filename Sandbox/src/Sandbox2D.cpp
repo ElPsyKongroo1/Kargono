a073182ef3ae2bbc619f80b30f42cfa805c645c8
Charles <cbreuer10@gmail.com>
2023-08-04 10:26:26 -0700

    Full Set up before DocSpace

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -58,9 +58,9 @@ void Sandbox2D::OnUpdate(Kargono::Timestep ts)
 		Kargono::Renderer2D::EndScene();
 
 		Kargono::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		for (float y = -5.0f; y < 5.0f; y += 0.2f)
 		{
-			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			for (float x = -5.0f; x < 5.0f; x += 0.2f)
 			{
 				glm::vec4 color = { x + 5.0f / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.5f};
 				Kargono::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
