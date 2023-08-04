c2c94cd2048c5bbdba2af6df37514af0f0c6d97d
Charles <cbreuer10@gmail.com>
2023-08-04 11:07:10 -0700

    Add ImGui Dockspace

--- Kargono/Kargono/Core/Application.h
+++ Kargono/Kargono/Core/Application.h
@@ -30,6 +30,8 @@ namespace Kargono
 
 		static Application& Get() { return *s_Instance; }
 		Window& GetWindow() { return *m_Window; }
+
+		void Close();
 	private:
 		void Run();
 		bool OnWindowClose(WindowCloseEvent& e);
