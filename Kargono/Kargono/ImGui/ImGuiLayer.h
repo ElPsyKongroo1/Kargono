c4f3be740e3720c529f1fcc04984069c354a5ed3
Charles <cbreuer10@gmail.com>
2023-08-04 20:17:07 -0700

    Fix Viewport Input Issues

--- Kargono/Kargono/ImGui/ImGuiLayer.h
+++ Kargono/Kargono/ImGui/ImGuiLayer.h
@@ -23,9 +23,10 @@ namespace Kargono
 		void Begin();
 		void End();
 		
-
+		void BlockEvents(bool block) { m_BlockEvents = block; }
 
 	private:
+		bool m_BlockEvents = true;
 		float m_Time = 0.0f;
 
 	};
