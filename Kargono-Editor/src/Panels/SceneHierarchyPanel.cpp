b6cd837e61feb5ee4c91507dd97b723958ce6a16
Charles <cbreuer10@gmail.com>
2023-08-11 14:46:32 -0700

    Update Scene Panel

--- Kargono-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Kargono-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -1,5 +1,7 @@
 #include "SceneHierarchyPanel.h"
 #include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Kargono/Scene/Components.h"
 
 namespace Kargono
@@ -19,10 +21,63 @@ namespace Kargono
 		m_Context->m_Registry.each([&](auto entityID)
 		{
 			Entity entity{entityID, m_Context.get()};
-			auto& tc = entity.GetComponent<TagComponent>();
-			ImGui::Text("%s", tc.Tag.c_str());
+			DrawEntityNode(entity);
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered()) { m_SelectionContext = {}; }
+
 		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext){ DrawComponents(m_SelectionContext);}
+
+		ImGui::End();
+	}
+	void SceneHierarchyPanel::DrawEntityNode(Entity entity)
+	{
+		auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+		ImGuiTreeNodeFlags flags = ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow;
+		bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
+		if (ImGui::IsItemClicked())
+		{
+			m_SelectionContext = entity;
+		}
+		if (opened)
+		{
+			ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow;
+			bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
+			if (opened)
+				ImGui::TreePop();
+			ImGui::TreePop();
+		}
+	}
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256] = {};
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
+			
+
+		}
 	}
 }
