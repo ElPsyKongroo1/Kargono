72ab93bbef4fd50559eed9e6e6155d169455a096
Charles <cbreuer10@gmail.com>
2023-08-11 18:03:04 -0700

    Add SpriteRenderer Component to SceneHierarchy Panel

--- Kargono-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Kargono-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -129,5 +129,16 @@ namespace Kargono
 				ImGui::TreePop();
 			}
 		}
+
+		if (entity.HasComponent<SpriteRendererComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Srpite Renderer"))
+			{
+				auto& src = entity.GetComponent<SpriteRendererComponent>();
+				ImGui::ColorEdit4("Color", glm::value_ptr(src.Color));
+
+				ImGui::TreePop();
+			}
+		}
 	}
 }
