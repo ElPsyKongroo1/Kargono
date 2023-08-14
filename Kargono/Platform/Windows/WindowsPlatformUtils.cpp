bf169951845412f4a44099cbcec41e38615725a3
Charles <cbreuer10@gmail.com>
2023-08-13 17:07:35 -0700

    Minor Fixes to Serialization

--- Kargono/Platform/Windows/WindowsPlatformUtils.cpp
+++ Kargono/Platform/Windows/WindowsPlatformUtils.cpp
@@ -1,5 +1,7 @@
 #include "Kargono/kgpch.h"
 #include "Kargono/Utils/PlatformUtils.h"
+
+#include <sstream>
 #include <commdlg.h>
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
@@ -9,7 +11,7 @@
 
 namespace Kargono
 {
-	std::string FileDialogs::OpenFile(const char* filter)
+	std::optional<std::string> FileDialogs::OpenFile(const char* filter)
 	{
 		OPENFILENAMEA ofn; // common dialog box structure
 		CHAR szFile[260] = { 0 }; // if using TCHAR macros
@@ -22,14 +24,11 @@ namespace Kargono
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
-		if (GetOpenFileNameA(&ofn) == TRUE)
-		{
-			return ofn.lpstrFile;
-		}
-		return std::string();
+		if (GetOpenFileNameA(&ofn) == TRUE) { return ofn.lpstrFile; }
+		return std::nullopt;
 	}
 
-	std::string FileDialogs::SaveFile(const char* filter)
+	std::optional<std::string> FileDialogs::SaveFile(const char* filter)
 	{
 		OPENFILENAMEA ofn; // common dialog box structure
 		CHAR szFile[260] = { 0 }; // if using TCHAR macros
@@ -42,10 +41,10 @@ namespace Kargono
 		ofn.lpstrFilter = filter;
 		ofn.nFilterIndex = 1;
 		ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
-		if (GetSaveFileNameA(&ofn) == TRUE)
-		{
-			return ofn.lpstrFile;
-		}
-		return std::string();
+
+		// Sets the default extension by extracting it from the filter
+		ofn.lpstrDefExt = strchr(filter, '\0') + 1;
+		if (GetSaveFileNameA(&ofn) == TRUE) { return ofn.lpstrFile; }
+		return std::nullopt;
 	}
 }
