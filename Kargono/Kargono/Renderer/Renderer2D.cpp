982ab094745e57cbdac6a3b95b0884e9432a7714
Charles <cbreuer10@gmail.com>
2023-08-31 10:51:20 -0700

    Fixed Graphics Bug

--- Kargono/Kargono/Renderer/Renderer2D.cpp
+++ Kargono/Kargono/Renderer/Renderer2D.cpp
@@ -476,11 +476,9 @@ namespace Kargono
 	}
 	void Renderer2D::DrawSprite(const glm::mat4& transform, SpriteRendererComponent& src, int entityID)
 	{
-		if (src.Texture)
-		{
-			DrawQuad(transform, src.Texture, src.TilingFactor, src.Color, entityID);
-		}
-		DrawQuad(transform, src.Color, entityID);
+		if (src.Texture){DrawQuad(transform, src.Texture, src.TilingFactor, src.Color, entityID);}
+		else { DrawQuad(transform, src.Color, entityID); }
+		
 	}
 	float Renderer2D::GetLineWidth()
 	{
