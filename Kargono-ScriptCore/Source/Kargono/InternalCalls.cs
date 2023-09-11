967037b977ad78de419844c826d5226a58ebf94e
ElPsy <elpsykongroo100@gmail.com>
2023-09-10 17:58:30 -0700

    Add Entity access of other entities in C#

--- Kargono-ScriptCore/Source/Kargono/InternalCalls.cs
+++ Kargono-ScriptCore/Source/Kargono/InternalCalls.cs
@@ -8,6 +8,13 @@ namespace Kargono
 	{
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		internal extern static bool Entity_HasComponent(ulong entityID, Type componentType);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		internal extern static ulong Entity_FindEntityByName(string name);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		internal extern static object GetScriptInstance(ulong entityID);
+
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		internal extern static void TransformComponent_GetTranslation(ulong entityID, out Vector3 parameter);
 
