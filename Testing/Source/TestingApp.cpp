8a6f7e8ce77b43dbf2f9a5224b01cb72a6cd825a
Charles-Breuer <cbreuer10@gmail.com>
2024-06-13 08:04:15 -0700

    Testing New Runner

--- Testing/Source/TestingApp.cpp
+++ Testing/Source/TestingApp.cpp
@@ -8,5 +8,7 @@ TEST_CASE("testing the factorial function")
     CHECK(factorial(1) == 0);
     CHECK(factorial(2) == 2);
     CHECK(factorial(3) == 6);
-    CHECK(factorial(10) == 3628800);
+	CHECK(factorial(10) == 3628800);
+	CHECK(factorial(1) == 1);
+
 }
