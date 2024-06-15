e6ead327ebd331178fb2ff95ccec96a84beaaa51
ElPsy <elpsykongroo100@gmail.com>
2024-06-15 14:25:15 -0700

    Add Comments to Engine/API

--- Engine/Source/API/ImageProcessing/stb_image_impl.cpp
+++ Engine/Source/API/ImageProcessing/stb_image_impl.cpp
@@ -1,8 +1,8 @@
 #include "kgpch.h"
-//============================================================
-// This file contains the implementation of the stb_image Dependency
-// The implementation gets added during the preprocessing stage
-//============================================================
+
+//==============================
+// Dependency Implementation
+//==============================
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
