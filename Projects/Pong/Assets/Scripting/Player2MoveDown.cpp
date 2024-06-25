7a30d428a6df9cd26796e2a0074a2a78da749e88
Charles Breuer <cbreuer10@gmail.com>
2024-06-24 22:28:22 -0700

    Continue work transitioning scripts to new system - Added FindEntityHandleByName to Scripting.cpp

--- Projects/Pong/Assets/Scripting/Player2MoveDown.cpp
+++ Projects/Pong/Assets/Scripting/Player2MoveDown.cpp
@@ -1,3 +1,16 @@
-void Player2MoveDown(uint64_t a,float b)
+void Player2MoveDown(uint64_t entity,float timeStep)
 {
+	Math::vec3 translation = TransformComponent_GetTranslation(entity);
+	bool upperLimit = translation.y >= 11.15;
+	bool lowerLimit = translation.y <= -11.15;
+
+	//float speed = Input.IsKeyDown(KeyCode.RightShift) ? Speed * SpeedUpFactor : Speed;
+	//Vector3 velocity = Vector3.Zero;
+
+	//if (!lowerLimit) { velocity.Y = -1.0f; }
+
+	//velocity *= speed * ts;
+	//Translation += velocity;
 }
+
+
