b5ae4d4b969a0687542e7adafd4ea984018bf027
Karl Johnson <cbreuer10@gmail.com>
2024-07-17 23:36:05 -0700

    Continue work on script compiler - Add support for tokenizing return, int literal, and semicolon

--- Engine/Source/Kargono/Scripting/ScriptCompiler.cpp
+++ Engine/Source/Kargono/Scripting/ScriptCompiler.cpp
@@ -28,17 +28,89 @@ namespace Kargono::Scripting
 
 		// Get Tokens from Text
 		std::vector<ScriptToken> allTokens = ConvertTextToTokens(scriptFile);
-		return scriptFile;
+
+		//TODO: Testing
+		std::stringstream outputStream {};
+		for (auto& token : allTokens)
+		{
+			outputStream << "TokenType: " << Utility::ScriptTokenTypeToString(token.Type) << " | Value: " << token.Value << "\n";
+		}
+
+		return outputStream.str();
 	}
 	std::vector<ScriptToken> ScriptCompiler::ConvertTextToTokens(const std::string& text)
 	{
-		uint32_t iteration = 0;
-		for (auto character : text)
-		{
+		std::vector<ScriptToken> newTokens {};
+		std::string tokenBuffer {};
 
-			KG_TRACE_CRITICAL(character);
-			iteration++;
+		uint32_t textLocation = 0;
+		while (textLocation < text.size())
+		{
+			if (std::isalpha(text.at(textLocation)))
+			{
+				// Add first character to buffer
+				tokenBuffer.push_back(text.at(textLocation));
+				textLocation++;
+
+				// Fill remainder of buffer
+				while (std::isalnum(text.at(textLocation)))
+				{
+					tokenBuffer.push_back(text.at(textLocation));
+					textLocation++;
+					if (textLocation >= text.size())
+					{
+						break;
+					}
+				}
+
+				// Check what type of token is provided
+				if (tokenBuffer == "return")
+				{
+					newTokens.push_back({ ScriptTokenType::Return, {} });
+					tokenBuffer.clear();
+					continue;
+				}
+				KG_WARN("Invalid token provided");
+			}
+
+			if (std::isdigit(text.at(textLocation)))
+			{
+				// Add first digit to buffer
+				tokenBuffer.push_back(text.at(textLocation));
+				textLocation++;
+
+				// Fill remainder of buffer
+				while (std::isdigit(text.at(textLocation)))
+				{
+					tokenBuffer.push_back(text.at(textLocation));
+					textLocation++;
+					if (textLocation >= text.size())
+					{
+						break;
+					}
+				}
+
+				// Fill in integer literal
+				newTokens.push_back({ ScriptTokenType::IntLiteral, tokenBuffer });
+				tokenBuffer.clear();
+				continue;
+			}
+
+			if (std::isspace(text.at(textLocation)))
+			{
+				textLocation++;
+				continue;
+			}
+
+			if (text.at(textLocation) == ';')
+			{
+				newTokens.push_back({ ScriptTokenType::SemiColon, {} });
+				textLocation++;
+				continue;
+			}
+
+			KG_ERROR("Could not identify character!");
 		}
-		return {};
+		return newTokens;
 	}
 }
--- Engine/Source/Kargono/Scripting/ScriptCompiler.h
+++ Engine/Source/Kargono/Scripting/ScriptCompiler.h
@@ -10,13 +10,12 @@ namespace Kargono::Scripting
 		None = 0,
 		Return,
 		IntLiteral,
-		SemiColon,
-
+		SemiColon
 	};
 
 	struct ScriptToken
 	{
-		ScriptTokenType TokenType{ ScriptTokenType::None };
+		ScriptTokenType Type{ ScriptTokenType::None };
 		std::string Value {};
 	};
 
@@ -36,4 +35,25 @@ namespace Kargono::Scripting
 		//==============================
 		static std::vector<ScriptToken> ConvertTextToTokens(const std::string& text);
 	};
+}
+
+namespace Kargono::Utility
+{
+	inline std::string ScriptTokenTypeToString(Scripting::ScriptTokenType type)
+	{
+		switch (type)
+		{
+			case Scripting::ScriptTokenType::Return: return "Return";
+			case Scripting::ScriptTokenType::IntLiteral: return "Integer Literal";
+			case Scripting::ScriptTokenType::SemiColon: return "Semicolon";
+
+			case Scripting::ScriptTokenType::None:
+			default:
+			{
+				KG_CRITICAL("Unknown Type of ScriptTokenType.");
+				return {};
+				
+			}
+		}
+	}
 }
