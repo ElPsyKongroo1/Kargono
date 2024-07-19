44f34dcc1632ae32e619d062b2587a5b55a30fd3
Karl Johnson <cbreuer10@gmail.com>
2024-07-19 00:19:01 -0700

    Add Grid to Editor - Added RoundUp and RoundDown functinos in Utility/Operations - Completed basic grid overlay in editor

--- Engine/Source/Kargono/Utility/Operations.h
+++ Engine/Source/Kargono/Utility/Operations.h
@@ -22,5 +22,31 @@ namespace Kargono::Utility
 		{
 			std::erase(string, character);
 		}
+		template <typename T>
+		static T RoundDown(T valueToRound, T multipleOf)
+		{
+			// Avoid division by zero
+			if (multipleOf == 0)
+			{
+				return valueToRound; 
+			}
+			return valueToRound - (valueToRound % multipleOf);
+		}
+		template <typename T>
+		static T RoundUp(T valueToRound, T multipleOf) {
+			// Avoid division by zero
+			if (multipleOf == 0)
+			{
+				return valueToRound;
+			}
+			T remainder = valueToRound % multipleOf;
+
+			// If valueToRound is already a multiple of multipleOf
+			if (remainder == 0)
+			{
+				return valueToRound; 
+			}
+			return valueToRound + (multipleOf - remainder);
+		}
 	};
 }
