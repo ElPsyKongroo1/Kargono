02e4ae0f15b073334a000fe11cfb5184c49a96fc
ElPsy <elpsykongroo100@gmail.com>
2025-05-13 20:39:38 -0700

    Refactor SystemAlloc and fix workflow issues - Move Linux and Windows implementation into their own files - Refactor SystemAlloc.h/.cpp to match engine style

--- Engine/Source/Kargono/Memory/SystemAlloc.h
+++ Engine/Source/Kargono/Memory/SystemAlloc.h
@@ -1,34 +1,33 @@
 #pragma once
 
+#include "Kargono/Memory/Platform/MemoryTypes.h"
+
 #include <cstdint>
 #include <cstdlib>
 
+
 namespace Kargono::Memory
 {
-	constexpr size_t MEM_ALIGN_4_KIB = 4 * 1024;
-	constexpr size_t MEM_ALIGN_2_MIB = 2 * 1024 * 1024;
-	constexpr size_t MEM_ALIGN_1_GIB = 1 * 1024 * 1024 * 1024;
+	constexpr size_t k_MemAlign4KIB = 4 * 1024;
+	constexpr size_t k_MemAlign2MIB = 2 * 1024 * 1024;
+	constexpr size_t k_MemAlign1GIB = 1 * 1024 * 1024 * 1024;
+
+	struct MirrorAllocResult
+	{
+		MemoryFileDesc m_MemoryFileDesc;
+		uint8_t* m_Data;
+		size_t m_Length;
+		size_t m_Capacity;
+	};
 
 	class System
 	{
 	public:
 		static uint8_t* GenAlloc(size_t dataSize, size_t alignment);
 		static uint8_t* PageAlloc(size_t dataSize, size_t alignment);
-
-		struct MirrorAllocResult {
-#if defined(_WIN32)
-# include <windows.h>
-			HANDLE memfd;
-#elif defined(__linux__)
-			int memfd;
-#else
-# error "Unknown platform, must be one of: windows linux"
-#endif
-			uint8_t* ptr;
-			size_t len, cap;
-		};
-
 		static MirrorAllocResult MirrorAlloc(size_t dataSize, size_t mirrors);
+	private:
+		static uint8_t* PageAllocHelper(size_t dataSize, size_t alignment);
 	};
 }
 
